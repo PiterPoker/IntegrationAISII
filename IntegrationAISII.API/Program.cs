98c3d9c54d779d970c5249539ae2f273ce371cc4
fashionman <tema5286@gmail.com>
2024-04-02 15:19:43 +0300

    feat: Added logging

--- IntegrationAISII.API/Program.cs
+++ IntegrationAISII.API/Program.cs
@@ -1,46 +1,126 @@
 using HealthChecks.UI.Client;
+using IntegrationAISII.API;
 using IntegrationAISII.API.Infrastructure;
 using IntegrationAISII.Infrastructure;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Serilog;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+//TODO: Дописать логирование и создать Startup
+string AppName = builder.Environment.ApplicationName;
 
-// Add services to the container.
-
-builder.Services
-    .AddCustomHealthCheck(builder.Configuration)
-    .AddCustomDbContext(builder.Configuration);
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    Log.Logger = CreateSerilogLogger(builder.Configuration);
+
+    Log.Information("Applying migrations ({ApplicationContext})...", AppName);
+
+    // Add services to the container.
+
+    builder.Services
+        .AddCustomHealthCheck(builder.Configuration)
+        .AddCustomDbContext(builder.Configuration);
+    builder.Services
+        .AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services
+        .AddEndpointsApiExplorer();
+    builder.Services
+        .AddSwaggerGen();
+
+    var app = builder.Build();
+
+    Log.Information("Configuring web host ({ApplicationContext})...", AppName);
+    LogPackagesVersionInfo();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    //app.MapControllers();
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+        endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+        endpoints.MapHealthChecksUI(config => config.UIPath = "/hc-ui");
+    });
+    Log.Information("Starting web host ({ApplicationContext})...", AppName);
+
+    app.Run();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+{
+    var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+    var logstashUrl = configuration["Serilog:LogstashgUrl"];
+    return new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .Enrich.WithProperty("ApplicationContext", AppName)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "https://seq" : seqServerUrl)
+        .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "https://logstash:8080" : logstashUrl, 1000)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
 
-//app.MapControllers();
-app.UseRouting();
-app.UseEndpoints(endpoints => { 
-    endpoints.MapControllers();
-    endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+string GetVersion(Assembly assembly)
+{
+    try
     {
-        Predicate = _ => true,
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
-    endpoints.MapHealthChecksUI(config => config.UIPath = "/hc-ui");
-});
+        return $"{assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version} ({assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split()[0]})";
+    }
+    catch
+    {
+        return string.Empty;
+    }
+}
 
-app.Run();
+void LogPackagesVersionInfo()
+{
+    var assemblies = new List<Assembly>();
+
+    foreach (var dependencyName in typeof(Program).Assembly.GetReferencedAssemblies())
+    {
+        try
+        {
+            // Try to load the referenced assembly...
+            assemblies.Add(Assembly.Load(dependencyName));
+        }
+        catch
+        {
+            // Failed to load assembly. Skip it.
+        }
+    }
+
+    var versionList = assemblies.Select(a => $"-{a.GetName().Name} - {GetVersion(a)}").OrderBy(value => value);
+
+    Log.Logger.ForContext("PackageVersions", string.Join("\n", versionList)).Information("Package versions ({ApplicationContext})", AppName);
+}
